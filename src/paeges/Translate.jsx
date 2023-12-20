e9f697eb69076cc2721b521aaf998a1550cdab38
MrPondzleep <Pondkub1324@gmail.com>
2023-12-20 15:24:30 +0700

    create
    create

--- src/paeges/Translate.jsx
+++ src/paeges/Translate.jsx
@@ -1,28 +1,19 @@
-import React from "react";
+import React from 'react'
 import "flowbite";
 
 function Translate() {
   return (
-    <div>
-      <div className="flex justify-center items-center h-screen bg-gray-100 min-h-screen p-4">
-        <center>
-          <a
-            href="https://translate.google.co.th/?hl=th&tab=rT"
-            class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-          >
-            Translate
-          </a>
-          <div className="My_Translate"> translate
-            <div className="box-translate">
-
-            </div>
-          </div>
-
-        </center>
-      </div>
-      <div />
-    </div>
-  );
+    
+  <div>
+     <div className="flex justify-center items-center h-screen bg-gray-100 min-h-screen p-4">
+    <center>
+<a href="https://translate.google.co.th/?hl=th&tab=rT" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Translate</a>
+</center>
+</div>
+<div/>
+</div>
+    
+  )
 }
 
-export default Translate;
+export default Translate
