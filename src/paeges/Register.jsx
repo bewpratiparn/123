ede42fc5921018a424b57599fa6050ce775a6db7
MrPondzleep <Pondkub1324@gmail.com>
2024-01-14 23:07:54 +0700

    translate

--- src/paeges/Register.jsx
+++ src/paeges/Register.jsx
@@ -10,7 +10,7 @@ function Register() {
           
           <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-              id
+              id {count}
             </label>
             <input
               className="w-full p-2 border rounded-md"
--- src/paeges/Translate.jsx
+++ src/paeges/Translate.jsx
@@ -1,9 +1,31 @@
+import { useEffect } from "react";
 import React from "react";
 import { BiDownvote } from "react-icons/bi";
 import "flowbite";
 import "./translate.css";
 
 function Translate() {
+
+  function MyComponent() {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+  useEffect(() => {
+    fetch("https://api.example.com/items")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])}
+
+  
   return (
     
     <div className="background">
