b87caefbe97a2684ab0cf60d9e5862d3082fafe0
MrPondzleep <Pondkub1324@gmail.com>
2024-01-16 22:32:13 +0700

    Update Translate.jsx

--- src/paeges/Translate.jsx
+++ src/paeges/Translate.jsx
@@ -1,72 +1,64 @@
-import { useEffect } from "react";
+import { useState } from "react";
 import React from "react";
 import { BiDownvote } from "react-icons/bi";
 import "flowbite";
 import "./translate.css";
 
 function Translate() {
+  /**function เพิ่มตัวเลขเฉยๆ ทดลองเขียน logic */
+  const [counter, setCounter] = useState(0);
 
-  function MyComponent() {
-    
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [items, setItems] = useState([]);
-  useEffect(() => {
-    fetch("https://api.example.com/items")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          setIsLoaded(true);
-          setItems(result);
-        },
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      )
-  }, [])}
+  const updateCounter = () => {
+    setCounter(counter + 1);
+  };
+
   return (
-    
     <div className="background">
-      <div className="Header">
-        Welcome to Translate
-      </div>
+      <div className="Header">Welcome to Translate</div>
       <center>
         <div className="color-Translate">
-          
           <div className="color-language">
-          <div className ="language">
-               <button><h1>EN</h1></button> 
-               <button><h1>TH</h1></button>
+            <div className="language">
+              <button>
+                <h1>EN</h1>
+              </button>
+              <button>
+                <h1>TH</h1>
+              </button>
             </div>
-            </div>                
-        <div className="box-InputTranslate">
-          <div className="Input-language">
-            <label for="textInput">Enter languange :</label>
-            <input
-              type="text"
-              id="textInput"
-              name="textInput"
-              placeholder="Type here..."
-            />
           </div>
-        </div>
-         
-          <div className="outline-button-translate">
-          <button className="button-trn" id="translateButton" onclick="translateText()"><h1>Translate</h1></button>
+          <div className="box-InputTranslate">
+            <div className="Input-language">
+              <label for="textInput">Enter languange :</label>
+              <input
+                type="text"
+                id="textInput"
+                name="textInput"
+                placeholder="Type here..."
+              />
+            </div>
           </div>
-       
-        <div className="box-OutputTranslate">
-        <div className="Input-language">
-            <label for="textOutput">Output languange :</label>
-            <input
-              type="text"
-              id="textOutput"
-              name="textOutput"
-              placeholder=""
-            />
+          <div>
+            {" "}
+            counter = {counter}
+            <div className="outline-button-translate">
+              <button className="button-trn" onClick={updateCounter}>
+                <h1>Translate</h1>
+              </button>
+            </div>
           </div>
-        </div>
+
+          <div className="box-OutputTranslate">
+            <div className="Input-language">
+              <label for="textOutput">Output languange :</label>
+              <input
+                type="text"
+                id="textOutput"
+                name="textOutput"
+                placeholder=""
+              />
+            </div>
+          </div>
         </div>
       </center>
     </div>
