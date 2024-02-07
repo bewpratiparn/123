8975b093dc9e5879ad9c750c59e0e072de94f033
MrPondzleep <Pondkub1324@gmail.com>
2024-02-07 14:14:29 +0700

    .

--- src/paeges/Translate.jsx
+++ src/paeges/Translate.jsx
@@ -1,67 +1,114 @@
-import { useState } from "react";
-import React from "react";
-import { BiDownvote } from "react-icons/bi";
-import "flowbite";
+import React, { useState, useEffect } from "react";
+import { Form, TextArea, Button, Icon } from "semantic-ui-react";
 import "./translate.css";
-
+import axios from "axios";
+import 'semantic-ui-css/semantic.min.css'
 function Translate() {
-  /**function เพิ่มตัวเลขเฉยๆ ทดลองเขียน logic */
-  const [counter, setCounter] = useState(0);
+  const [inputText, setInputText] = useState('');
+  const [detectLanguageKey, setdetectedLanguageKey] = useState('');
+  const [selectedLanguageKey, setLanguageKey] = useState('');
+  const [languagesList, setLanguagesList] = useState([]);
+  const [resultText, setResultText] = useState('');
+  const [apiKey, setApiKey] = useState('');
 
-  const updateCounter = () => {
-    setCounter(counter + 1);
-  };
+  const languageOptions = [
+      {key: 'en', Text: 'English', value:'male'},
+      {key: 'hi ', Text: 'Hindi', value:'female'},
+      {key: 'ar', Text: 'Arabic', value:'other'},
+      {key: 'fr', Text: 'French', value:'other'},
+      {key: 'es', Text: 'Spanish', value:'other'},
+      {key: 'bg', Text: 'Bangla', value:'other'},
+      {key: 'ur', Text: 'Arabic', value:'other'},
+      {key: 'gr', Text: 'Greek', value:'other'},
+
+  ]
+
+  const getLanguageSource = () => {
+    axios
+      .post(`https://libretranslate.com/translate`, {
+        q: inputText,
+      })
+      .then((response) => {
+        setdetectedLanguageKey(response.data[0].language);
+      })
+  }
+  
+  useEffect(() => {
+    axios
+      .get("https://libretranslate.de/languages")
+      .then((response) => {
+      setLanguagesList(response.data)
+    })
+
+    getLanguageSource();
+  }, [inputText])
+
+  
+  const languageKey = (selectedLanguage) => {
+    setLanguageKey(selectedLanguage.target.value)
+  }
+  const translateText = () => {
+    getLanguageSource();
+
+    let data = {
+      q: inputText,
+      source: detectLanguageKey,
+      target: selectedLanguageKey,
+    }
+    axios
+    .post(`https://libretranslate.de/translate`, data)
+    .then((response) => {
+      setResultText(response.data.translatedText)
+    })
+  }
 
   return (
-    <div className="background">
-      <div className="Header">Welcome to Translate</div>
-      <center>
-        <div className="color-Translate">
-          <div className="color-language">
-            <div className="language">
-              <button>
-                <h1>EN</h1>
-              </button>
-              <button>
-                <h1>TH</h1>
-              </button>
-            </div>
-          </div>
-          <div className="box-InputTranslate">
-            <div className="Input-language">
-              <label for="textInput">Enter languange :</label>
-              <input
-                type="text"
-                id="textInput"
-                name="textInput"
-                placeholder="Type here..."
-              />
-            </div>
-          </div>
-          <div>
-            counter = {counter}
-            <div className="outline-button-translate">
-              <button className="button-trn" onClick={updateCounter}>
-                <h1>Translate</h1>
-              </button>
-            </div>
-          </div>
+    <div>
+      <div className="app-header">
+        <h2 className="header"> Translator</h2>
+      </div>
 
-          <div className="box-OutputTranslate">
-            <div className="Input-language">
-              <label for="textOutput">Output languange :</label>
-              <input
-                type="text"
-                id="textOutput"
-                name="textOutput"
-                placeholder=""
-              />
-            </div>
-          </div>
+
+      <div className="app-body">
+        <div>
+          <Form>
+            <Form.Field
+              control={TextArea}
+              label='About'
+              placeholder="Type Text to Translate.."
+              onChange={(e) => setInputText(e.target.value)}
+            />
+
+            <select className="language-select" onChange={languageKey}>
+              <option>Please Select Language..</option>
+              {languageOptions.map((language) => {
+                return( 
+                <option value={language.code}>
+                  {language.name}
+                  </option>
+                )
+              })}
+            </select>
+
+            <Form.Field
+              control={TextArea}
+              placeholder="Your Result Translation.."
+              value={resultText}
+            />
+
+            <Button 
+            color="orange" 
+            size="large" 
+            onClick={translateText}
+
+            >
+           <Icon name="translate" />
+              Translate
+            </Button>
+          </Form>
         </div>
-      </center>
+      </div>
     </div>
   );
 }
-
 export default Translate;
