6452e0202cdaa1dca3774ac98d3d5de5ad16bb5a
MrPondzleep <Pondkub1324@gmail.com>
2024-04-19 00:24:05 +0700

    ปอนด์อัพเดต

--- src/components/NavbarNew.jsx
+++ src/components/NavbarNew.jsx
@@ -31,14 +31,14 @@ function NavbarNew() {
                 className="w-5 h-5 mr-2 rounded-full"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
-                xmlns:xlink="http://www.w3.org/1999/xlink"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
                 viewBox="0 0 3900 3900"
               >
                 <path fill="#b22234" d="M0 0h7410v3900H0z" />
                 <path
                   d="M0 450h7410m0 600H0m0 600h7410m0 600H0m0 600h7410m0 600H0"
                   stroke="#fff"
-                  stroke-width="300"
+                  strokeWidth="300"
                 />
                 <path fill="#3c3b6e" d="M0 0h2964v2100H0z" />
                 <g fill="#fff">
@@ -50,19 +50,19 @@ function NavbarNew() {
                             id="a"
                             d="M247 90l70.534 217.082-184.66-134.164h228.253L176.466 307.082z"
                           />
-                          <use xlink:href="#a" y="420" />
-                          <use xlink:href="#a" y="840" />
-                          <use xlink:href="#a" y="1260" />
+                          <use xlinkHref="#a" y="420" />
+                          <use xlinkHref="#a" y="840" />
+                          <use xlinkHref="#a" y="1260" />
                         </g>
-                        <use xlink:href="#a" y="1680" />
+                        <use xlinkHref="#a" y="1680" />
                       </g>
-                      <use xlink:href="#b" x="247" y="210" />
+                      <use xlinkHref="#b" x="247" y="210" />
                     </g>
-                    <use xlink:href="#c" x="494" />
+                    <use xlinkHref="#c" x="494" />
                   </g>
-                  <use xlink:href="#d" x="988" />
-                  <use xlink:href="#c" x="1976" />
-                  <use xlink:href="#e" x="2470" />
+                  <use xlinkHref="#d" x="988" />
+                  <use xlinkHref="#c" x="1976" />
+                  <use xlinkHref="#e" x="2470" />
                 </g>
               </svg>
               English (US)
@@ -87,8 +87,8 @@ function NavbarNew() {
                         id="flag-icon-css-us"
                         viewBox="0 0 512 512"
                       >
-                        <g fill-rule="evenodd">
-                          <g stroke-width="1pt">
+                        <g fillRule="evenodd">
+                          <g strokeWidth="1pt">
                             <path
                               fill="#bd3d44"
                               d="M0 0h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0z"
@@ -154,9 +154,9 @@ function NavbarNew() {
               >
                 <path
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M1 1h15M1 7h15M1 13h15"
                 />
               </svg>
@@ -250,9 +250,9 @@ function NavbarNew() {
                           >
                             <path
                               stroke="currentColor"
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              stroke-width="2"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
                               d="m1 1 4 4 4-4"
                             />
                           </svg>
@@ -306,9 +306,9 @@ function NavbarNew() {
                           >
                             <path
                               stroke="currentColor"
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              stroke-width="2"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
                               d="m1 1 4 4 4-4"
                             />
                           </svg>
@@ -370,9 +370,9 @@ function NavbarNew() {
                           >
                             <path
                               stroke="currentColor"
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              stroke-width="2"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
                               d="m1 1 4 4 4-4"
                             />
                           </svg>
--- src/paeges/AddDataShop.jsx
+++ src/paeges/AddDataShop.jsx
@@ -1,169 +1,223 @@
 /*ปอนด์ Do*/
-import React,{useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./AddDataShop.css";
+import axios from "axios";
 
 function AddDataShop() {
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+  const [isUploading, setIsUploading] = useState(false);
+  const [isUploaded, setIsUploaded] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [uploadedFile, setUploadedFile] = useState(null);
 
-    const formData = new FormData(event.target);
-    const data = {
-      name: formData.get("name"),
-      email: formData.get("email"),
-    };
+  const [formData, setformData] = useState({
+    storename: "",
+    location: "",
+    tel: "",
+    maplink: "",
+    onclose: "",
+  });
 
-    try {
-      const response = await fetch("", {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: { "Content-Type": "application/json" },
+  useEffect(() => {
+    axios
+      .get(
+        "http://127.0.0.1:8000/docs#/default/register_shop_shops_register__post"
+      )
+      .then((response) => {
+        const { storename, location, tel, maplink, onclose } = response.data;
+        setformData({ storename, location, tel, maplink, onclose });
+      })
+      .catch((error) => {
+        console.error("error fetching user data", error);
       });
+  }, []);
 
-      if (response.ok) {
-        // Handle success
-      } else {
-        // Handle error
-      }
-    } catch (error) {
-      // Handle error
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(formData);
+
+    setformData({
+      storename: "",
+      location: "",
+      tel: "",
+      maplink: "",
+      onclose: "",
+    });
+  };
+  const handleFileChange = (e) => {
+    const btnUpload = e.target;
+    const ext = btnUpload.value.split('.').pop().toLowerCase();
+    if (!['gif', 'png', 'jpg', 'jpeg'].includes(ext)) {
+      setErrorMessage('Not an Image...');
+    } else {
+      setErrorMessage('');
+      setIsUploading(true);
+      setTimeout(() => {
+        setIsUploading(false);
+        setIsUploaded(true);
+      }, 3000);
+      const uploadedFileURL = URL.createObjectURL(btnUpload.files[0]);
+      setTimeout(() => {
+        setUploadedFile(uploadedFileURL);
+      }, 3500);
     }
   };
+
+  const handleFileRemove = () => {
+    setIsUploaded(false);
+    setUploadedFile(null);
+    setIsUploading(false);
+    setErrorMessage('');
+  };
   return (
-    
     <>
-    <div className="form-center">
-      <form>
-        <div className="flex flex-col justify-center items-center m-10 ">
-          <div className="m-5 text-center ">เพิ่มข้อมูลร้านค้า</div>
-          <div className="button_outer">
-				<div className="btn_upload">
-					<input type="file" id="upload_file" name />
-					Upload Image
-				</div>
-				<div class="processing_bar"></div>
-				<div class="success_box"></div>
-			</div>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2 ">
-              ชื่อร้าน
-            </label>
-            <input
-              className=" p-2 border rounded-md"
-              name="storename"
-              type="text"
-              placeholder="กรอกชื่อร้าน"
-            />
-          </div>
-
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-3 width ">
-              สถานที่
-            </label>
-            <input
-              className=" p-2 border rounded-md"
-              name="location"
-              type="text"
-              placeholder="กรอกสถานที่"
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2 width ">
-              เบอร์โทรศัพท์
-            </label>
-            <input
-              className=" p-2 border rounded-md"
-              name=""
-              type="text"
-              placeholder="กรอกเบอร์โทรศัพท์"
-            />
-          </div>
-
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2 width ">
-              Map-link
-            </label>
-            <input
-              className=" p-2 border rounded-md"
-              name="username"
-              type="text"
-              placeholder="กรอก map link"
-            />
-          </div>
-
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2 width ">
-              เวลา เปิด-ปิด
-            </label>
-            <input
-              className=" p-2 border rounded-md"
-              name="username"
-              type="text"
-              placeholder="เวลา เปิด-ปิด"
-            />
-          </div>
-          <div className="grid grid-rows-3 gap-2">
-            <div>
-              <label className="inline-flex items-center mb-2">
-                <input
-                  type="checkbox"
-                  className="form-checkbox text-blue-600"
-                />
-                <span className="ml-2 text-black">เพิ่มมังสวิรัติ</span>
-                <img
-                  src="https://media.istockphoto.com/id/1311125920/th/%E0%B9%80%E0%B8%A7%E0%B8%84%E0%B9%80%E0%B8%95%E0%B8%AD%E0%B8%A3%E0%B9%8C/%E0%B8%9C%E0%B8%A5%E0%B8%B4%E0%B8%95%E0%B8%A0%E0%B8%B1%E0%B8%93%E0%B8%91%E0%B9%8C%E0%B8%A1%E0%B8%B1%E0%B8%87%E0%B8%AA%E0%B8%A7%E0%B8%B4%E0%B8%A3%E0%B8%B1%E0%B8%95%E0%B8%B4-100-%E0%B9%80%E0%B8%9B%E0%B8%AD%E0%B8%A3%E0%B9%8C%E0%B9%80%E0%B8%8B%E0%B9%87%E0%B8%99%E0%B8%95%E0%B9%8C-%E0%B9%82%E0%B8%A5%E0%B9%82%E0%B8%81%E0%B9%89%E0%B8%AA%E0%B8%B5%E0%B9%80%E0%B8%82%E0%B8%B5%E0%B8%A2%E0%B8%A7%E0%B8%A1%E0%B8%B1%E0%B8%87%E0%B8%AA%E0%B8%A7%E0%B8%B4%E0%B8%A3%E0%B8%B1%E0%B8%95%E0%B8%B4-%E0%B8%AA%E0%B8%B1%E0%B8%8D%E0%B8%A5%E0%B8%B1%E0%B8%81%E0%B8%A9%E0%B8%93%E0%B9%8C-eco-bio-%E0%B9%81%E0%B8%A5%E0%B8%B0-organic.jpg?s=612x612&w=0&k=20&c=FvYOIGvyyb941-1vz-_s2ZGgOUz-AZMEJ5zW8yFhU5g="
-                  alt="มังสวิรัติ"
-                  style={{ width: "70px", height: "auto" }} // กำหนดขนาดภาพเป็น 100px กว้างและปรับความสูงตามอัตราส่วน
-                />
+      <div className="form-center">
+        <form onSubmit={handleSubmit}>
+          <div className="flex flex-col justify-center items-center m-10 ">
+            <div className="m-5 text-center ">เพิ่มข้อมูลร้านค้า</div>
+            <div className="containeruploadfile">
+              <input className="" type="file" id="upload_file" onChange={handleFileChange} />
+              <div
+                className={
+                  isUploading ? "button_outer file_uploading" : "button_outer"
+                }
+               
+              >
+                {isUploaded && (
+                  <div id="uploaded_view" className="show">
+                    <img src={uploadedFile} alt="Uploaded File" />
+                  </div>
+                )}
+              </div>
+              {errorMessage && <div className="error_msg">{errorMessage}</div>}
+              <button className="file_remove" onClick={handleFileRemove}>
+                Remove
+              </button>
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2 ">
+                ชื่อร้าน
               </label>
+              <input
+                className="p-2 border rounded-md"
+                name="storename"
+                type="text"
+                placeholder="กรอกชื่อร้าน"
+              />
+            </div>
+
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-3 width ">
+                สถานที่
+              </label>
+              <input
+                className=" p-2 border rounded-md"
+                name="location"
+                type="text"
+                placeholder="กรอกสถานที่"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2 width ">
+                เบอร์โทรศัพท์
+              </label>
+              <input
+                className=" p-2 border rounded-md"
+                name="tel"
+                type="text"
+                placeholder="กรอกเบอร์โทรศัพท์"
+              />
+            </div>
+
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2 width ">
+                Map-link
+              </label>
+              <input
+                className=" p-2 border rounded-md"
+                name="maplink"
+                type="text"
+                placeholder="กรอก map link"
+              />
+            </div>
+
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2 width ">
+                เวลา เปิด-ปิด
+              </label>
+              <input
+                className=" p-2 border rounded-md"
+                name="onclose"
+                type="text"
+                placeholder="เวลา เปิด-ปิด"
+              />
+            </div>
+            <div className="grid grid-rows-3 gap-2">
+              <div>
+                <label className="inline-flex items-center mb-2">
+                  <input
+                    type="checkbox"
+                    name="mungsavirat"
+                    className="form-checkbox text-blue-600"
+                  />
+                  <span className="ml-2 text-black">เพิ่มมังสวิรัติ</span>
+                  <img
+                    src="https://media.istockphoto.com/id/1311125920/th/%E0%B9%80%E0%B8%A7%E0%B8%84%E0%B9%80%E0%B8%95%E0%B8%AD%E0%B8%A3%E0%B9%8C/%E0%B8%9C%E0%B8%A5%E0%B8%B4%E0%B8%95%E0%B8%A0%E0%B8%B1%E0%B8%93%E0%B8%91%E0%B9%8C%E0%B8%A1%E0%B8%B1%E0%B8%87%E0%B8%AA%E0%B8%A7%E0%B8%B4%E0%B8%A3%E0%B8%B1%E0%B8%95%E0%B8%B4-100-%E0%B9%80%E0%B8%9B%E0%B8%AD%E0%B8%A3%E0%B9%8C%E0%B9%80%E0%B8%8B%E0%B9%87%E0%B8%99%E0%B8%95%E0%B9%8C-%E0%B9%82%E0%B8%A5%E0%B9%82%E0%B8%81%E0%B9%89%E0%B8%AA%E0%B8%B5%E0%B9%80%E0%B8%82%E0%B8%B5%E0%B8%A2%E0%B8%A7%E0%B8%A1%E0%B8%B1%E0%B8%87%E0%B8%AA%E0%B8%A7%E0%B8%B4%E0%B8%A3%E0%B8%B1%E0%B8%95%E0%B8%B4-%E0%B8%AA%E0%B8%B1%E0%B8%8D%E0%B8%A5%E0%B8%B1%E0%B8%81%E0%B8%A9%E0%B8%93%E0%B9%8C-eco-bio-%E0%B9%81%E0%B8%A5%E0%B8%B0-organic.jpg?s=612x612&w=0&k=20&c=FvYOIGvyyb941-1vz-_s2ZGgOUz-AZMEJ5zW8yFhU5g="
+                    alt="มังสวิรัติ"
+                    style={{ width: "70px", height: "auto" }} // กำหนดขนาดภาพเป็น 100px กว้างและปรับความสูงตามอัตราส่วน
+                  />
+                </label>
+              </div>
+
+              <div>
+                <label className="inline-flex items-center mb-2">
+                  <input
+                    type="checkbox"
+                    name="jay"
+                    className="form-checkbox text-blue-600"
+                  />
+                  <span className="ml-2 text-black">เพิ่มอาหารเจ</span>
+                  <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRS-b1-Nxx_cbgUySvFd2emKL8rQwR39810zYZp2U9PMgLXojLNnR8XlPJcUXEEs4ucfq0&usqp=CAU"
+                    alt="อาหารเจ"
+                    style={{ width: "70px", height: "auto" }} // กำหนดขนาดภาพเป็น 100px กว้างและปรับความสูงตามอัตราส่วน
+                  />
+                </label>
+              </div>
+
+              <div>
+                <label className="inline-flex items-center  mb-4 ">
+                  <input
+                    type="checkbox"
+                    name="halan"
+                    className="form-checkbox text-blue-600"
+                  />
+                  <span className="ml-2 text-black ">เพิ่มฮาลาน</span>
+                  <img
+                    src="https://assets.brandinside.asia/uploads/2017/09/HALAL.jpg"
+                    alt="ฮาลาน"
+                    style={{ width: "70px", height: "auto" }} // กำหนดขนาดภาพเป็น 100px กว้างและปรับความสูงตามอัตราส่วน
+                  />
+                </label>
+              </div>
             </div>
 
             <div>
-              <label className="inline-flex items-center mb-2">
-                <input
-                  type="checkbox"
-                  className="form-checkbox text-blue-600"
-                />
-                <span className="ml-2 text-black">เพิ่มอาหารเจ</span>
-                <img
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRS-b1-Nxx_cbgUySvFd2emKL8rQwR39810zYZp2U9PMgLXojLNnR8XlPJcUXEEs4ucfq0&usqp=CAU"
-                  alt="อาหารเจ"
-                  style={{ width: "70px", height: "auto" }} // กำหนดขนาดภาพเป็น 100px กว้างและปรับความสูงตามอัตราส่วน
-                />
-              </label>
-            </div>
-
-            <div>
-              <label className="inline-flex items-center  mb-4 ">
-                <input
-                  type="checkbox"
-                  className="form-checkbox text-blue-600"
-                />
-                <span className="ml-2 text-black ">เพิ่มฮาลาน</span>
-                <img
-                  src="https://assets.brandinside.asia/uploads/2017/09/HALAL.jpg"
-                  alt="ฮาลาน"
-                  style={{ width: "70px", height: "auto" }} // กำหนดขนาดภาพเป็น 100px กว้างและปรับความสูงตามอัตราส่วน
-                />
-              </label>
+              <button
+                type="button"
+                className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+              >
+                ยืนยัน
+              </button>
+              <button
+                type="submit"
+                className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+              >
+                ยกเลิก
+              </button>
             </div>
           </div>
-
-          <div>
-            <button
-              type="button"
-              class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-            >
-              ยืนยัน
-            </button>
-            <button
-              type="button"
-              class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-            >
-              ยกเลิก
-            </button>
-          </div>
-        </div>
-      </form>
+        </form>
       </div>
     </>
   );
