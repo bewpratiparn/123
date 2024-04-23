b2b5185851c881a270f8284a99c60f6b84062302
bewpratiparn <117655970+bewpratiparn@users.noreply.github.com>
2024-04-24 04:23:59 +0700

    up

--- src/paeges/Store_information.jsx
+++ src/paeges/Store_information.jsx
@@ -7,7 +7,7 @@ function Editstore() {
     const [data, setData] = useState([]);
 
     useEffect(() => {
-        axios.get('http://127.0.0.1:8000/shops/')
+        axios.get('http://127.0.0.1:8000/foods/')
             .then(res => setData(res.data))
             .catch(err => {
                 Swal.fire({
@@ -19,52 +19,36 @@ function Editstore() {
             });
     }, []);
 
-
-
-
     return (
-        <>
-
-
-
-
-            <div style={{
-                marginLeft: '0px',
-                marginRight: '0px',
-                marginTop: '0px',
-                backgroundColor: 'white',
-                width: '300vw',
-                height: '300vh',
-            }}>
-
-
-
-<div>
+        <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            backgroundColor: 'white',
+            width: '100vw',
+            height: '250vh',
+        }}>
+            <div>
                 <div>
                     <img
-                        src="https://www.southernliving.com/thmb/dvvxHbEnU5yOTSV1WKrvvyY7clY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-1205217071-2000-2a26022fe10b4ec8923b109197ea5a69.jpg "
+                        src="https://www.southernliving.com/thmb/dvvxHbEnU5yOTSV1WKrvvyY7clY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-1205217071-2000-2a26022fe10b4ec8923b109197ea5a69.jpg"
                         className="picture"
                     />
                 </div>
-
-                <div style={{ marginLeft: '220px', marginTop: '160px', display: 'flex', justifyContent: 'flex-start', fontSize: '24px', fontWeight: 'bold' }}>
-                </div>
-
-                <div style={{ marginLeft: '580px', marginTop: '-250px', justifyContent: 'flex-start', fontSize: '14px' }}>
-
-                    <label className="dayoff" htmlFor="ชื่อร้านค้า">
+                <div style={{ marginTop: '160px' }}> </div>
+                   
+               
+                <div style={{ marginLeft: '550px', marginTop: '-250px', fontSize: '16px' }}>
+                <label className="dayoff" htmlFor="ชื่อร้านค้า">
                         ชื่อร้านค้า :
                     </label>
                     <br />
                     <label className="dayoff" htmlFor="ชื่อร้านค้า">
-                        วันที่,เวลา เปิด-ปิด
+                        วันที่,เวลา เปิด-ปิด :
                     </label>
-                    <br />
-
+                    
                 </div>
-
-                <div style={{ marginLeft: '200px', marginTop: '200px', display: 'flex', justifyContent: 'flex-start', fontSize: '14px' }}>
-                    <div className="containner-box">
+                <div style={{ marginLeft: '200px', marginTop: '200px', fontSize: '15px' }}>
+                <div className="containner-box">
                         <div className="colorinside">
                             <label className="description" htmlFor="description">Description</label>
                             <div className="location">สถานที่ ชื่อสถานที่ :</div>
@@ -73,99 +57,33 @@ function Editstore() {
                         </div>
                     </div>
                 </div>
-</div>
+           
 
-
-
-
-                <div className="grid-item">
-
-                    <div>
-                        <img
-                            src="https://www.southernliving.com/thmb/dvvxHbEnU5yOTSV1WKrvvyY7clY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-1205217071-2000-2a26022fe10b4ec8923b109197ea5a69.jpg "
-                            className="picture-menu"
-                        />
-                        <div>ชื่ออาหาร :</div>
-                        <div>ราคา : </div>
-                    </div>
-                    <div>
-                        <img
-                            src="https://www.southernliving.com/thmb/dvvxHbEnU5yOTSV1WKrvvyY7clY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-1205217071-2000-2a26022fe10b4ec8923b109197ea5a69.jpg "
-                            className="picture-menu"
-                        />
-                        <div>ชื่ออาหาร :</div>
-                        <div>ราคา : </div>
-                    </div>
-                    <div>
-                        <img
-                            src="https://www.southernliving.com/thmb/dvvxHbEnU5yOTSV1WKrvvyY7clY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-1205217071-2000-2a26022fe10b4ec8923b109197ea5a69.jpg "
-                            className="picture-menu"
-                        />
-                        <div>ชื่ออาหาร :</div>
-                        <div>ราคา : </div>
-                    </div>
-                    <div>
-                        <img
-                            src="https://www.southernliving.com/thmb/dvvxHbEnU5yOTSV1WKrvvyY7clY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-1205217071-2000-2a26022fe10b4ec8923b109197ea5a69.jpg "
-                            className="picture-menu"
-                        />
-                        <div>ชื่ออาหาร :</div>
-                        <div>ราคา : </div>
-                    </div>
-                    <div>
-                        <img
-                            src="https://www.southernliving.com/thmb/dvvxHbEnU5yOTSV1WKrvvyY7clY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-1205217071-2000-2a26022fe10b4ec8923b109197ea5a69.jpg "
-                            className="picture-menu"
-                        />
-                        <div>ชื่ออาหาร :</div>
-                        <div>ราคา : </div>
-                    </div>
-                    <div>
-                        <img
-                            src="https://www.southernliving.com/thmb/dvvxHbEnU5yOTSV1WKrvvyY7clY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-1205217071-2000-2a26022fe10b4ec8923b109197ea5a69.jpg "
-                            className="picture-menu"
-                        />
-                        <div>ชื่ออาหาร :</div>
-                        <div>ราคา : </div>
-                    </div>
-                    <div>
-                        <img
-                            src="https://www.southernliving.com/thmb/dvvxHbEnU5yOTSV1WKrvvyY7clY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-1205217071-2000-2a26022fe10b4ec8923b109197ea5a69.jpg "
-                            className="picture-menu"
-                        />
-                        <div>ชื่ออาหาร :</div>
-                        <div>ราคา : </div>
-                    </div>
-                    <div>
-                        <img
-                            src="https://www.southernliving.com/thmb/dvvxHbEnU5yOTSV1WKrvvyY7clY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-1205217071-2000-2a26022fe10b4ec8923b109197ea5a69.jpg "
-                            className="picture-menu"
-                        />
-                        <div>ชื่ออาหาร :</div>
-                        <div>ราคา : </div>
-                    </div>
-                    <div>
-                        <img
-                            src="https://www.southernliving.com/thmb/dvvxHbEnU5yOTSV1WKrvvyY7clY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-1205217071-2000-2a26022fe10b4ec8923b109197ea5a69.jpg "
-                            className="picture-menu"
-                        />
-                        <div>ชื่ออาหาร :</div>
-                        <div>ราคา : </div>
-                    </div>
-
-
-
-
-                </div>
-
-
-
-
-
-
+            
+           
+             <div className="grid-item" style={{ marginLeft: '-90px' , padding: 10 ,marginTop: '-120px',fontSize: '16px',display: 'grid', gridTemplateColumns: 'repeat(2, 20fr)', gap: '8rem' }}>
+                {data.length > 0
+                    ? data.map((item, i) => (
+                        <div className="containner-store-1" key={item.id}>
+                            <div className="card" style={{ width: "100%" }}>
+                                <img
+                                    src={item.Food_picture}
+                                    alt={item.Food_name}
+                                    style={{ width: "100%", margin: "1rem" }}
+                                />
+                                <div>ชื่ออาหาร: {item.Food_name}</div>
+                                <div>ราคา: {item.Food_price}</div>
+                            </div>
+                        </div>
+                    ))
+                    : ""}
+            </div>
+          
             </div>
 
-        </>
+
+
+        </div>
     );
 }
 
