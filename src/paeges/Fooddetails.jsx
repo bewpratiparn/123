6892c40df6e70a0355a8af07675c13f2a1f0226d
bewpratiparn <117655970+bewpratiparn@users.noreply.github.com>
2024-05-03 23:25:16 +0700

    ีup

--- src/paeges/Fooddetails.jsx
+++ src/paeges/Fooddetails.jsx
@@ -1,55 +1,40 @@
 import React from 'react';
 import './Fooddetails.css';
 
-
 function Fooddetails() {
   return (
-    <>
-
-
-
-    
-<div style={{ marginLeft: '40px', marginRight: '40px', backgroundColor: 'gray', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-<div style={{  marginLeft: '0px', marginRight: '40px',fontSize: '24px', fontWeight: 'bold', color: 'black', textAlign: 'left' }}>หน้าแสดงรายละเอียดอาหาร</div>
-
-  <div style={{ marginTop: '10px' ,display: 'flex', justifyContent: 'flex-start', fontSize: '19px' }}>
-  <div style={{ display: 'flex', alignItems: 'center', marginRight: '450px' }}>ชื่ออาหาร : </div>
-  <div style={{ display: 'flex', alignItems: 'center', marginRight: '450px' }}>ราคา : </div>
-
-</div>
-
-      <div style={{ display: 'flex', alignItems: 'center', marginTop: '20px' , justifyContent: 'center', }}>
-  <img src="https://i.ytimg.com/vi/XyYMbh2uay4/maxresdefault.jpg" alt="Food Image" style={{ width: '20%', marginTop: '10px' }} />
-  <div style={{ marginLeft: '20px'  ,fontSize: '19px'}}>
-    <div>
-      <p>วัตถุดิบ 
-      <input type="text" id="ingredients1" name="ingredients1" style={{ marginLeft: '200px', marginTop: '20px', padding: '80px', width: '900px', }} />
-        
-        
-      </p>
+    <div className="container">
+      <div className="title">หน้าแสดงรายละเอียดอาหาร</div>
+      <div className="details">
+        <div className="detail-food">ชื่ออาหาร : </div>
+        <div className="detail-price">ราคา : </div>
+      </div>
+      <div className="image-container">
+        <img
+          src="https://i.ytimg.com/vi/XyYMbh2uay4/maxresdefault.jpg"
+          alt="Food Image"
+          className="food-image"
+        />
+        <div className="ingredients-container">
+          <div className="ingredients-label"> วัตถุดิบ </div>
+              <input
+                type="text"
+                id="ingredients1"
+                name="ingredients1"
+                className="ingredients-input"
+              />
+        </div>
+      </div>
+      <div>
+        <div className="description">รายละเอียดอาหาร</div>
+        <input
+          type="text"
+          id="ingredients1"
+          name="ingredients1"
+          className="description-input"
+        />
+      </div>
     </div>
-    
-      
-    </div>
-  
-</div>
-
-
-
-
-
-<div>
-  <div style={{ marginTop: '30px', marginLeft: '30px', fontSize: '19px', marginRight: '6px', display: 'inline-block' }}>รายละเอียดอาหาร</div>
-  <input type="text" id="ingredients1" name="ingredients1" style={{ marginLeft: '200px', marginTop: '20px', padding: '80px', width: '900px', }} />
-  </div>
- 
-  </div>
-
-
-
-
-
-    </>
   );
 }
 
