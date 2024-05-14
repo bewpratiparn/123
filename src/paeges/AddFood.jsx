236dccd85aa0f879df45a2773318349d27d886f0
bewpratiparn <117655970+bewpratiparn@users.noreply.github.com>
2024-05-14 18:38:14 +0700

    up

--- src/paeges/AddFood.jsx
+++ src/paeges/AddFood.jsx
@@ -1,73 +1,158 @@
-import React, { useState } from 'react';
-import './AddFood.css';
-import Swal from 'sweetalert2'; // import Swal เข้ามา
+import React, { useState, useEffect } from "react";
+import Navbar from "../components/Navbar";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import "./AddFood.css";
 
 function AddFood() {
-  const [formData, setFormData] = useState({
-    image: '',
-    foodName: '',
-    price: '',
-    subject: ''
+  const navigate = useNavigate();
+  const MySwal = withReactContent(Swal);
+  const [input, setInputs] = useState({
+    Food_name: "",
+    Food_element: "",
+    Food_price: "",
+    Food_picture: "",
   });
 
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setInputs((prevInputs) => ({
+      ...prevInputs,
+      [name]: value,
+    }));
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const userToken = localStorage.getItem("token");
 
-    try {
-      const response = await fetch('http://127.0.0.1:8000/add_food/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(formData)
+    if (userToken) {
+      const headers = {
+        Authorization: `Bearer ${userToken}`,
+        "Content-Type": "application/json",
+      };
+
+      const raw = JSON.stringify({
+        Food_name: input.Food_name,
+        Food_element: input.Food_element,
+        Food_price: input.Food_price,
+        Food_picture: input.Food_picture,
       });
 
-      if (response.ok) {
-        // ส่งข้อมูลสำเร็จ
-        Swal.fire({
-          icon: 'success',
-          title: 'Success',
-          text: 'Food added successfully!'
+      axios
+        .post("http://127.0.0.1:8000/add_food/", raw, { headers })
+        .then((response) => {
+          if (response.data.message === "Food data added successfully") {
+            MySwal.fire({
+              html: <i>{response.data.message}</i>,
+              icon: "success",
+            }).then(() => {
+              navigate("/Home");
+            });
+          } else {
+            MySwal.fire({
+              html: <i>เพิ่มข้อมูลร้านค้าสำเร็จ</i>,
+              icon: "error",
+            });
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+          MySwal.fire({
+            html: <i>เกิดข้อผิดพลาดในการเพิ่มข้อมูลร้านค้า</i>,
+            icon: "error",
+          });
         });
-      } else {
-        // ไม่สามารถส่งข้อมูลได้
-        throw new Error('Failed to add food');
-      }
-    } catch (error) {
-      console.error('Error:', error);
-      Swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: 'Failed to add food. Please try again later.'
+    } else {
+      MySwal.fire({
+        html: <i>โปรดล็อกอิน</i>,
+        icon: "warning",
       });
     }
   };
 
   return (
-    <div>
-      <h3>เพิ่มข้อมูลอาหาร</h3>
-      <div className="container">
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="image">เพิ่มรูปภาพ</label>
-          <input type="file" id="image" name="image" accept="image/*" onChange={handleChange} />
-          <br></br>
-          <label htmlFor="foodName">ชื่ออาหาร</label>
-          <input type="text" id="foodName" name="foodName" placeholder="Enter food name.." onChange={handleChange} />
-          <br></br>
-          <label htmlFor="price">ราคาอาหาร</label>
-          <input type="text" id="price" name="price" placeholder="Enter price.." onChange={handleChange} />
-          <br></br>
-          <label htmlFor="subject">รายละเอียดอาหาร</label>
-          <textarea id="subject" name="subject" placeholder="Write something.." style={{ height: '200px' }} onChange={handleChange}></textarea>
-          <br></br>
-          <input type="submit" value="Submit" />
-        </form>
+    <>
+      <div className="background">
+        <div className="flex flex-col justify-center items-center m-10">
+          <div className="m-5 text-center">เพิ่มข้อมูลอาหาร</div>
+          <form onSubmit={handleSubmit}>
+            <div className="grid gap-6 mb-6 md:grid-cols-2">
+              <div>
+                <div className="mb-4">
+                  <label className="block text-gray-700 text-sm font-bold mb-2">
+                    ชื่อเมนู
+                  </label>
+                  <input
+                    className="p-2 border rounded-md"
+                    name="Food_name"
+                    type="text"
+                    placeholder="ชื่อเมนู..."
+                    value={input.Food_name}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="mb-4">
+                  <label className="block text-gray-700 text-sm font-bold mb-2">
+                    องค์ประกอบอาหาร
+                  </label>
+                  <input
+                    className="p-2 border rounded-md w-96"
+                    name="Food_element"
+                    type="text"
+                    placeholder="องค์ประกอบของอาหาร"
+                    value={input.Food_element}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="mb-4">
+                  <label className="block text-gray-700 text-sm font-bold mb-2">
+                    ราคา
+                  </label>
+                  <input
+                    className="p-2 border rounded-md"
+                    name="Food_price"
+                    type="text"
+                    placeholder="ราคา..."
+                    value={input.Food_price}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="mb-4">
+                  <label className="block text-gray-700 text-sm font-bold mb-2">
+                    โปรดเพิ่มรูปอาหารของท่าน
+                  </label>
+                  <input
+                    className="p-2 border rounded-md"
+                    name="Food_picture"
+                    type="file"
+                    placeholder="รูปภาพ..."
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+            </div>
+            <div>
+              <button
+                type="submit"
+                className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+              >
+                ยืนยัน
+              </button>
+              <button
+                type="reset"
+                className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+              >
+                ยกเลิก
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
