7ab3925f67c667e167131f06ce9be1119f2b9666
bewpratiparn <117655970+bewpratiparn@users.noreply.github.com>
2024-05-20 23:53:26 +0700

    up

--- src/paeges/AddDataShop.jsx
+++ src/paeges/AddDataShop.jsx
@@ -88,6 +88,18 @@ function AddDataShop() {
     }
   };
 
+  const handleReset = () => {
+    setAddShop({
+      pictureshop: "",
+      storename: "",
+      location: "",
+      phone: "",
+      onclose: "",
+      shop_type: "",
+    });
+    setImageURL("");
+  };
+
   return (
     <div className="background">
       <div className="flex flex-col justify-center items-center m-10">
@@ -160,8 +172,6 @@ function AddDataShop() {
                   onChange={handleChange}
                 />
               </div>
-            </div>
-            <div>
               <div className="mb-4">
                 <label className="block text-gray-700 text-sm font-bold mb-2">
                   เพิ่มรูปร้านค้า
@@ -186,23 +196,22 @@ function AddDataShop() {
           <div>
             <button
               type="submit"
-              className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm
-              px-5 py-2.5 text-center mr-2 mb-2
-              "
-              >
-                ยืนยัน
-              </button>
-              <button
-                type="reset"
-                className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-              >
-                ยกเลิก
-              </button>
-            </div>
-          </form>
-        </div>
+              className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+            >
+              ยืนยัน
+            </button>
+            <button
+              type="button"
+              onClick={handleReset}
+              className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+            >
+              ยกเลิก
+            </button>
+          </div>
+        </form>
       </div>
-    );
-  }
-  
-  export default AddDataShop;
+    </div>
+  );
+}
+
+export default AddDataShop;
